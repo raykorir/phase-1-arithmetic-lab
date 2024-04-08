afce639b7db59cbdb5907f755b3d84d4c13f5ecd
raymond <raymond.korir@student.moringaschool.com>
2024-04-09 00:19:09 +0300

    arithmethic-lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random() * 100); 
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+const numbers = [10, 5, 20, 15];
+const max = Math.max(...numbers); 
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max
+};
